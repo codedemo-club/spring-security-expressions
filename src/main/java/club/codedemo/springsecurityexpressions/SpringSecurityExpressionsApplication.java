ddfe06d9e6be47ab87d7220a4cf7ae3355e64776
panjie <3792535@qq.com>
2020-08-02 16:31:39 +0800

    init

---
+++ src/main/java/club/codedemo/springsecurityexpressions/SpringSecurityExpressionsApplication.java
@@ -0,0 +1,13 @@
+package club.codedemo.springsecurityexpressions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityExpressionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityExpressionsApplication.class, args);
+	}
+
+}
