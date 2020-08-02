221d9d20f434a300baf0caad1352826bdb95dc7e
panjie <3792535@qq.com>
2020-08-02 16:51:54 +0800

    init

--- src/main/java/club/codedemo/springsecurityexpressions/controller/AuthController.java
+++ src/main/java/club/codedemo/springsecurityexpressions/controller/AuthController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/admin")
-public class AdminController {
+@RequestMapping("/auth")
+public class AuthController {
 
     @RequestMapping("/test")
     public String test() {
